321b17f5c35abd262fe728d4fbc5640456d3c8ae
asaadjabr <jabr.assaad@gmail.com>
2024-07-13 08:28:35 +0300

    Fix some errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,17 +5,7 @@ import { fetchCustomers } from '@/app/lib/data';
 export default async function Page() {
   return (
     <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Edit Invoice',
-            href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
-        ]}
-      />
-      <Form invoice={invoice} customers={customers} />
+
     </main>
   );
 }
