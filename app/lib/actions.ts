af2b6d349bd2c8bc929ed17609afc7beac917eeb
asaadjabr <jabr.assaad@gmail.com>
2024-07-15 08:42:24 +0300

    Install lint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,19 +15,67 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({id:true, date:true})
 
 export async function createInvoice (formData: FormData) {
-    const {customerId, amount, status} = CreateInvoice.parse({
+    
+        const {customerId, amount, status} = CreateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status')
+    })
+    
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    try {
+    await sql `
+    INSERT INTO Invoices(customer_id, amount, status, date)
+    VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`
+
+    revalidatePath('/dasshboard/invoices')
+
+
+    } catch(err) {
+        return `Database Error: Failed to Create Invoice`
+    }
+
+    redirect('/dashboard/invoices')
+
+}
+
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+
+    const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
-        status: formData.get('status')
-})
-
-const amountInCents = amount * 100;
-const date = new Date().toISOString().split('T')[0];
-
-await sql `
-INSERT INTO Invoices(customer_id, amount, status, date)
-VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`
-
-revalidatePath('/dasshboard/invoices')
-redirect('/dashboard/invoices')
+        status: formData.get('status'),
+      });
+     
+      const amountInCents = amount * 100;
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+     
+      revalidatePath('/dashboard/invoices');
+} catch(err) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
 }
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    
+try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return {message: 'Invoice deleted'}
+} catch(err) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+}
+  }
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -24,6 +24,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              
             >
               <option value="" disabled>
                 Select a customer
