7a1902f16738e4a6fb70ee1e55c0364c7298c86a
asaadjabr <jabr.assaad@gmail.com>
2024-07-16 16:43:12 +0300

    Authentication added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,39 +3,58 @@ import {z} from 'zod';
 import {sql} from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+ 
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({invalid_type_error: 'Please select a customer.'}),
+    amount: z.coerce.number().gt(0, {message: 'Please enter amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {invalid_type_error:'Please select an invoice status.'}),
     date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({id:true, date:true})
 
-export async function createInvoice (formData: FormData) {
+export async function createInvoice (prevState: State, formData: FormData) {
     
-        const {customerId, amount, status} = CreateInvoice.parse({
+        const validatedFields = CreateInvoice.safeParse({
             customerId: formData.get('customerId'),
             amount: formData.get('amount'),
             status: formData.get('status')
     })
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
     
+    const {customerId,amount, status} = validatedFields.data
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
     try {
     await sql `
     INSERT INTO Invoices(customer_id, amount, status, date)
     VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`
 
-    revalidatePath('/dasshboard/invoices')
-
-
-    } catch(err) {
-        return `Database Error: Failed to Create Invoice`
     }
-
+    catch(err) {
+        return {message:' Database Error: Failed to Create Invoice'}
+    }
+    revalidatePath('/dasshboard/invoices')
     redirect('/dashboard/invoices')
 
 }
@@ -46,15 +65,26 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice( id: string, prevState: State, formData: FormData) {
 
-    const { customerId, amount, status } = UpdateInvoice.parse({
+ const validatedFields= UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
-     
+
+      if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Create Invoice.',
+        };
+      }
+
+      const { customerId, amount, status } = validatedFields.data
       const amountInCents = amount * 100;
+
+
+
     try {
       await sql`
         UPDATE invoices
@@ -62,10 +92,11 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
      
-      revalidatePath('/dashboard/invoices');
+      
 } catch(err) {
     return { message: 'Database Error: Failed to Update Invoice.' };
 }
+revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
@@ -78,4 +109,23 @@ try {
 } catch(err) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
 }
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
